92e8293e01b08a17094f92ce806e94aba03652a0
Naufal Rezky Ananda <naufalrezkyananda08@gmail.com>
2023-01-09 09:01:17 +0000

    Update Figure

--- imagenette/read_and_plot_grouped.py
+++ imagenette/read_and_plot_grouped.py
@@ -22,11 +22,11 @@ batch_top1_top5_size64 = {}
 
 
 short_batch = '128'
-current_p = 'batch_acc_val'
-current_title = 'Batch related Validation top 5 Accuracy'
+current_p = 'val_top5'
+current_title = 'Epoch related Validation top 5 Accuracy'
 process = '_validationtop5_'
 
-with open('resnet18/resnet18_batch_'+short_batch+'_gsize_1_epo_50') as json_file:
+with open('resnet101/resnet101_batch_'+short_batch+'_gsize_1_epo_50') as json_file:
     data = json.load(json_file)
     for k,v in data.items():
         if k == current_p:
@@ -37,36 +37,36 @@ with open('resnet18/resnet18_batch_'+short_batch+'_gsize_1_epo_50') as json_file
             epochs = data[k]	
         elif k == 'args.batch_size':
             batch_size = data[k]
-with open('resnet18/resnet18_batch_'+short_batch+'_gsize_2_epo_50') as json_file:
+with open('resnet101/resnet101_batch_'+short_batch+'_gsize_2_epo_50') as json_file:
     data = json.load(json_file)
     for k,v in data.items():
         if k == current_p:
             batch_top1_top5_size2 = data[k]
-with open('resnet18/resnet18_batch_'+short_batch+'_gsize_4_epo_50') as json_file:
+with open('resnet101/resnet101_batch_'+short_batch+'_gsize_4_epo_50') as json_file:
     data = json.load(json_file)
     for k,v in data.items():
         if k == current_p:
             batch_top1_top5_size4 = data[k]
-with open('resnet18/resnet18_batch_'+short_batch+'_gsize_8_epo_50') as json_file:
+with open('resnet101/resnet101_batch_'+short_batch+'_gsize_8_epo_50') as json_file:
     data = json.load(json_file)
     for k,v in data.items():
         if k == current_p:
             batch_top1_top5_size8 = data[k]
-# with open('resnet18/resnet18_batch_'+short_batch+'_gsize_16_epo_50') as json_file:
-#     data = json.load(json_file)
-#     for k,v in data.items():
-#         if k == current_p:
-#             batch_top1_top5_size16 = data[k]
-# with open('resnet18/resnet18_batch_'+short_batch+'_gsize_32_epo_50') as json_file:
-#     data = json.load(json_file)
-#     for k,v in data.items():
-#         if k == current_p:
-#             batch_top1_top5_size32 = data[k]
-# with open('resnet18/resnet18_batch_'+short_batch+'_gsize_64_epo_50') as json_file:
-#     data = json.load(json_file)
-#     for k,v in data.items():
-#         if k == current_p:
-#             batch_top1_top5_size64 = data[k]
+with open('resnet101/resnet101_batch_'+short_batch+'_gsize_16_epo_50') as json_file:
+    data = json.load(json_file)
+    for k,v in data.items():
+        if k == current_p:
+            batch_top1_top5_size16 = data[k]
+with open('resnet101/resnet101_batch_'+short_batch+'_gsize_32_epo_50') as json_file:
+    data = json.load(json_file)
+    for k,v in data.items():
+        if k == current_p:
+            batch_top1_top5_size32 = data[k]
+with open('resnet101/resnet101_batch_'+short_batch+'_gsize_64_epo_50') as json_file:
+    data = json.load(json_file)
+    for k,v in data.items():
+        if k == current_p:
+            batch_top1_top5_size64 = data[k]
 
 
 def helper(batches):
@@ -79,23 +79,23 @@ def helper(batches):
     return (batch_range, batch_top1, batch_top5)
 
 
-x1, top1s1, top5s1 = helper(batch_top1_top5)
-x2, top1s2, top5s2 = helper(batch_top1_top5_size2)
-x4, top1s4, top5s4 = helper(batch_top1_top5_size4)
-x8, top1s8, top5s8 = helper(batch_top1_top5_size8)
+# x1, top1s1, top5s1 = helper(batch_top1_top5)
+# x2, top1s2, top5s2 = helper(batch_top1_top5_size2)
+# x4, top1s4, top5s4 = helper(batch_top1_top5_size4)
+# x8, top1s8, top5s8 = helper(batch_top1_top5_size8)
 # x16, top1s16, top5s16 = helper(batch_top1_top5_size16)
 # x32, top1s32, top5s32 = helper(batch_top1_top5_size32)
 # x64, top1s64, top5s64 = helper(batch_top1_top5_size64)
 
-"""
+
 epoch_range = range(1,epochs+1)
-plt.plot(epoch_range, train_top1, label = 'groupsize_1')
-plt.plot(epoch_range, train_top1_size2, label = 'groupsize_2')
-plt.plot(epoch_range, train_top1_size4, label = 'groupsize_4')
-plt.plot(epoch_range, train_top1_size8, label = 'groupsize_8')
-plt.plot(epoch_range, train_top1_size16, label = 'groupsize_16')
-plt.plot(epoch_range, train_top1_size32, label = 'groupsize_32')
-plt.plot(epoch_range, train_top1_size64, label = 'groupsize_64')
+plt.plot(epoch_range, batch_top1_top5, label = 'groupsize_1')
+plt.plot(epoch_range, batch_top1_top5_size2, label = 'groupsize_2')
+plt.plot(epoch_range, batch_top1_top5_size4, label = 'groupsize_4')
+plt.plot(epoch_range, batch_top1_top5_size8, label = 'groupsize_8')
+plt.plot(epoch_range, batch_top1_top5_size16, label = 'groupsize_16')
+plt.plot(epoch_range, batch_top1_top5_size32, label = 'groupsize_32')
+plt.plot(epoch_range, batch_top1_top5_size64, label = 'groupsize_64')
 plt.title(current_title)
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
@@ -104,18 +104,18 @@ plt.legend()
 plt.show(block=True) 
 ep = str(epochs)
 bat = str(batch_size)
-results_dir = 'grouped_model_graph/'
+results_dir = 'resnet101/'
 sample_file_name = model+"_"+current_p + "_batch_" + bat +"_epo_"+ ep +".png" 
 plt.savefig(results_dir + sample_file_name)
 
 """
-plt.plot(x1, top5s1, label = 'groupsize_1')
-plt.plot(x1, top5s2, label = 'groupsize_2')
-plt.plot(x1, top5s4, label = 'groupsize_4')
-plt.plot(x1, top5s8, label = 'groupsize_8')
-# plt.plot(x1, top1s16, label = 'groupsize_16')
-# plt.plot(x1, top1s32, label = 'groupsize_32')
-# plt.plot(x1, top1s64, label = 'groupsize_64')
+plt.plot(x1, top1s1, label = 'groupsize_1')
+plt.plot(x1, top1s2, label = 'groupsize_2')
+plt.plot(x1, top1s4, label = 'groupsize_4')
+plt.plot(x1, top1s8, label = 'groupsize_8')
+plt.plot(x1, top1s16, label = 'groupsize_16')
+plt.plot(x1, top1s32, label = 'groupsize_32')
+plt.plot(x1, top1s64, label = 'groupsize_64')
 plt.title(current_title)
 plt.xlabel('Batch')
 plt.ylabel('Accuracy')
@@ -124,7 +124,7 @@ plt.legend()
 plt.show(block=True)
 ep = str(epochs)
 bat = str(batch_size)
-results_dir = 'resnet18/'
+results_dir = 'grouped_model_graph/'
 sample_file_name = "batch_graph_"+ model +process+ "_batch_" + bat +"_epo_"+ ep +".png" 
 plt.savefig(results_dir + sample_file_name)
-#"""
+"""
