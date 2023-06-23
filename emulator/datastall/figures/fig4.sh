9cce910b3c61c748ed94037cf7d6833d76ee4ac0
Gus Waldspurger <gus@waldspurger.com>
2023-06-23 11:05:05 -0700

    update fig4 for minio

--- emulator/datastall/figures/fig4.sh
+++ emulator/datastall/figures/fig4.sh
@@ -3,15 +3,10 @@ set -e
 
 gpu_type="p100"
 model="resnet18"
-limits=("4G" "6G" "8G" "10G" "12G" "14G" "16G")
+limits=(4 6 8 10 12 14 16)
 batch_size="256"
 n_workers="4"
 data_path="/home/cc/data/test-utilization/imagenette2"
-
-# set up control group
-group_name="gpufs"
-sudo cgcreate -g memory:$group_name
-sudo chown -R ${USER} /sys/fs/cgroup/memory/$group_name
 
 # move the the emulator/datastall/ folder
 cd ..
@@ -19,16 +14,13 @@ cd ..
 for limit in ${limits[@]}; do
     echo "Profiling model $model with $limit GB memory limit"
 
-    # place the limit
-    sudo bash -c "echo $limit > /sys/fs/cgroup/memory/$group_name/memory.limit_in_bytes"
-
     # flush memory & caches
     echo "Flushing memory/cache"
     sudo ~/gpufs/exp/clear-cache.sh
 
     # run training with limited memory (https://unix.stackexchange.com/questions/44985/limit-memory-usage-for-a-single-linux-process)
     echo "running training"
-    cgexec -g memory:$group_name python main-measure-time-emulator.py --emulator-version=1 -j 4 --epoch 2 --workers $n_workers --gpu-count 1 --gpu-type $gpu_type -a $model --batch-size $batch_size --profile-batches -1 $data_path
+    python main-measure-time-emulator.py --emulator-version=1 -j 4 --epoch 2 -c $(($limit * 1024 * 1024 * 1024)) --workers $n_workers --gpu-count 1 --gpu-type $gpu_type -a $model --batch-size $batch_size --profile-batches -1 $data_path
 	
     # check how much memory the dataset was actually using
     # NOTE this isn't entirely accurate since the amount can vary throughout, and the amount at the end may not be representative/precise/etc. 
@@ -41,6 +33,4 @@ for limit in ${limits[@]}; do
     echo
 done
 
-# tear down the control group
-sudo cgdelete memory:$group_name
 
