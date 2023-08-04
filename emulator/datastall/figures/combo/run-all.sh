259016848928a6bb2c49a964d1fac99bdcb89545
Gus Waldspurger <gus@waldspurger.com>
2023-08-03 18:45:47 -0700

    update run all script logging

--- emulator/datastall/figures/combo/run-all.sh
+++ emulator/datastall/figures/combo/run-all.sh
@@ -10,12 +10,18 @@ echo ""
 
 echo "Running (1) -- Minio"
 ./run-minio.sh
+echo "(1) Minio done"
 
 echo "Running (2) -- Async"
 ./run-async.sh
+echo "(2) Async done"
 
 echo "Running (3) -- Minio + Async"
 ./run-minio-async.sh
+echo "(3) Minio + Async done"
 
 echo "Running (4) -- Default"
 ./run-normal.sh
+echo "(4) Default done"
+
+echo "... all runs completed"
