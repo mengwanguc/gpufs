e86134b6b9b07408fed158a1838e34cc86d281b4
Chameleon Cloud User <cc@gpufs-naufal-rtx6000-ffcv.novalocal>
2023-09-15 07:03:37 +0000

    emulating ffcv with normalize and convert

---
+++ ffcv/figure2/produce_figure_2.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+
+categories = ["ImageFolder Data Reading Only", "ImageFolder Reading + Processing", "ImageFolder Full Training", "Idealized Training","FFCV"]
+values = [35, 310, 327, 21 ,109]  
+# values = [34, 311, 330, 24 ,82] 
+
+plt.figure(figsize=(8, 4))  # Set the figure size (optional)
+
+plt.barh(categories, values)  # Create the horizontal bar chart
+
+plt.xlabel("Time per epoch (sec)")
+# plt.ylabel("Categories")
+plt.title("Time taken per set of stages in ImageNet training (on CPU with emulator)")
+
+# Save the chart as a JPEG image
+plt.savefig("figure-2-cpu.jpg", format="jpg", dpi=300, bbox_inches="tight")
+
+plt.show()  # Display the chart (optional)
+
+# ~~~~~~~~~~~~~~~~~~~~
+
+# categories = ["ImageFolder Data Reading Only", "ImageFolder Reading + Processing", "ImageFolder Full Training", "Idealized Training","FFCV"]
+# # values = [35, 310, 327, 21 ,109] cpu 
+# values = [34, 311, 330, 24 ,82] 
+
+# plt.figure(figsize=(8, 4))  # Set the figure size (optional)
+
+# plt.barh(categories, values)  # Create the horizontal bar chart
+
+# plt.xlabel("Time per epoch (sec)")
+# # plt.ylabel("Categories")
+# plt.title("Time taken per set of stages in ImageNet training (on GPU)")
+
+# # Save the chart as a JPEG image
+# plt.savefig("figure-2-gpu.jpg", format="jpg", dpi=300, bbox_inches="tight")
+
+# plt.show()  # Display the chart (optional)
+
+
+
