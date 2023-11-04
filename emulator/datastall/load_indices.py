1a44946a3f8e792b4e023eddaa94fd1296262d95
Gus Waldspurger <gus@waldspurger.com>
2023-11-04 13:51:46 -0500

    remove start/end logging

--- emulator/datastall/load_indices.py
+++ emulator/datastall/load_indices.py
@@ -121,19 +121,13 @@ def load_indices_async_minio_BACK(cache, user_state, dataset, batched_indices):
 ## LADCACHE ##
 
 def load_indices_ladcache_FRONT(cache, user_state, dataset, indices):
-    print("[PID {}] START load_indices front".format(os.getpid()))
-
     # Request all of the images.
     for index in indices:
         path, _ = dataset.samples[index]
         queue_depth, in_flight = user_state.get_queue_depth(), user_state.get_in_flight()
         user_state.submit(path, retry=False)
-    
-    print("[PID {}] END load_indices front".format(os.getpid()))
 
 def load_indices_ladcache_BACK(cache, user_state, dataset, indices):
-    print("[PID {}] START load_indices back".format(os.getpid()))
-
     # Determine where all of the images belong.
     targets = {}
     for index in indices:
@@ -147,8 +141,6 @@ def load_indices_ladcache_BACK(cache, user_state, dataset, indices):
         filepath = entry.get_filepath().decode()
         data.append((targets[filepath], entry.get_data()))
         del entry # releases the entry
-    
-    print("[PID {}] END load_indices back".format(os.getpid()))
 
     return data
 
