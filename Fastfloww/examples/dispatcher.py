11bad01d697a5e06c8f04a63346bfe3ca59dd792
NaufalRezkyA <56570051+NaufalRezkyA@users.noreply.github.com>
2023-11-14 20:39:51 +0700

    add dispatcher.py

---
+++ Fastfloww/examples/dispatcher.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+d_config = tf.data.experimental.service.DispatcherConfig(port=5000)
+dispatcher = tf.data.experimental.service.DispatchServer(d_config)
+
+w_port = 5001
+w_config = tf.data.experimental.service.WorkerConfig(
+    dispatcher_address=dispatcher.target.split("://")[1],
+    worker_address="10.140.82.140" + ":" + str(w_port),
+    port=w_port)
+worker = tf.data.experimental.service.WorkerServer(w_config)
+
+dispatcher.join()
