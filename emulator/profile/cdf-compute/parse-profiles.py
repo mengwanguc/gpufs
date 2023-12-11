8c4b1ba442657f4f02373ac34bcde8bb8452578d
Meng Wang <mengwanguc@gmail.com>
2023-12-10 19:17:46 -0600

    avg compute time parser

--- emulator/profile/cdf-compute/parse-profiles.py
+++ emulator/profile/cdf-compute/parse-profiles.py
@@ -10,30 +10,30 @@ gpu_type = sys.argv[1]
 models=['alexnet']
 
 num_profile_batches = 50
-batch_sizes = range()
+batch_sizes = range(1,177)
 
-with open("{}/avg.txt".format(gpu_type), "w") as f:
-    for model in models:
-        f.write("model: {}\n".format(model))
-        cpu2gpu_times_per_batchsize = []
-        gpu_times_per_batchsize = []
-        cpu2gpu_times = []
-        gpu_times = []
-        file_path = "{}/{}-batch{}.csv".format(gpu_type, model, batch_size)
-        if os.path.exists(file_path):
-            with open(file_path, 'r') as fread:
-                lines = fread.readlines()
-                for i in range(1, num_profile_batches+2):
-                    line = lines[i]
-                    if i > 5:
-                        numbers = line.strip().split('\t')
-                        cpu2gpu_times.append(numbers[0])
-                        gpu_times.append(numbers[1])
-        else:
-            print("{} does not exist!".format(file_path))
-            exit(0)
-        f.write('gpu_time\n')
-        for gpu_time in gpu_times:
+
+for model in models:
+    with open("{}/{}-avg.txt".format(gpu_type, model), "w") as f:
+        f.write('batch_size\tgpu_time\n')
+        for batch_size in batch_sizes:
+            cpu2gpu_times = []
+            gpu_times = []
+            file_path = "{}/{}-batch{}.csv".format(gpu_type, model, batch_size)
+            if os.path.exists(file_path):
+                with open(file_path, 'r') as fread:
+                    lines = fread.readlines()
+                    for i in range(1, num_profile_batches+2):
+                        line = lines[i]
+                        if i > 5:
+                            numbers = line.strip().split('\t')
+                            cpu2gpu_times.append(float(numbers[0]))
+                            gpu_times.append(float(numbers[1]))
+            else:
+                print("{} does not exist!".format(file_path))
+                exit(0)
+            avg_gpu_time = sum(gpu_times)/len(gpu_times)
+            f.write('{}\t{}\n'.format(batch_size, avg_gpu_time))
 
 
 
